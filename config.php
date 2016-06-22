b9a5b5062ccfba390f172b70d35dcde354067a05
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-06-22 14:57:43 -0400

    Config

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$title="Instituto Tecnologico DE ESTUDIOS CALIFICADOS";
+$siglas="";
+$directory="recursoshumanos";//ej:carp/ sin el "/"
+$url="http://localhost/";//ej:http:"localhost/"
+
+$ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
+$tp="D3W1N8E0E4B4B4";
+?>
