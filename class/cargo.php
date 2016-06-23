4457d09a50a6fa5afc79211acf8be07ee5c7a939
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-06-23 07:18:04 -0400

    Clases

---
+++ class/cargo.php
@@ -0,0 +1,6 @@
+<?php
+include_once("bd.php");
+class cargo extends bd{
+	var $tabla="cargo";
+}
+?>
---
+++ class/permiso.php
@@ -0,0 +1,6 @@
+<?php
+include_once("bd.php");
+class permiso extends bd{
+	var $tabla="permiso";
+}
+?>
