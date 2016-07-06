d4f63202aa6381912e369f194d81bac131bf640f
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-07-06 02:23:44 -0400

    Asistenci

--- asistencia/registro/guardar.php
+++ asistencia/registro/guardar.php
@@ -14,96 +14,63 @@ if( $_FILES['excel']['size']<="500000000"){
 	//mensaje que no es valido el tipo de archivo	
 	$mensaje[]="Archivo no válido. Verifique e intente nuevamente";
 }
-$excel="Formato Ingreso.xlsx";
+//$excel="Formato Ingreso.xls";
 include_once("../php-excel-reader/excel_reader2.php");
 include_once("../php-excel-reader/SpreadsheetReader.php");
 
 $data = new SpreadsheetReader("../excel/".$excel);
-/*echo "<pre>";
-print_r($data);
-echo "</pre>";
-$BaseMem = memory_get_usage();
-
-		$Sheets = $data -> Sheets();
-
-		echo '---------------------------------'.PHP_EOL;
-		echo 'Spreadsheets:'.PHP_EOL;
-		print_r($Sheets);
-		echo '---------------------------------'.PHP_EOL;
-		echo '---------------------------------'.PHP_EOL;
-
-		foreach ($Sheets as $Index => $Name)
-		{
-			echo '---------------------------------'.PHP_EOL;
-			echo '*** Sheet '.$Name.' ***'.PHP_EOL;
-			echo '---------------------------------'.PHP_EOL;
-
-			$Time = microtime(true);
-
-			$data -> ChangeSheet($Index);
-
-			foreach ($data as $Key => $Row)
-			{
-				echo $Key.': ';
-				if ($Row)
-				{
-					print_r($Row);
-				}
-				else
-				{
-					var_dump($Row);
-				}
-				$CurrentMem = memory_get_usage();
-		
-				echo 'Memory: '.($CurrentMem - $BaseMem).' current, '.$CurrentMem.' base'.PHP_EOL;
-				echo '---------------------------------'.PHP_EOL;
-		
-				if ($Key && ($Key % 500 == 0))
-				{
-					echo '---------------------------------'.PHP_EOL;
-					echo 'Time: '.(microtime(true) - $Time);
-					echo '---------------------------------'.PHP_EOL;
-				}
-			}
-		
-			echo PHP_EOL.'---------------------------------'.PHP_EOL;
-			echo 'Time: '.(microtime(true) - $Time);
-			echo PHP_EOL;
-
-			echo '---------------------------------'.PHP_EOL;
-			echo '*** End of sheet '.$Name.' ***'.PHP_EOL;
-			echo '---------------------------------'.PHP_EOL;
-		}*/
-/*
-$valores=array(	"materno"=>"'$materno'",
-				"paterno"=>"'$paterno'",
-				"nombres"=>"'$nombres'",
-				"fechanac"=>"'$fechanac'",
-				"ci"=>"'$ci'",
-				"sexo"=>"'$sexo'",
-				"direccion"=>"'$direccion'",
-				
-				"telefono"=>"'$telefono'",
-				"celular"=>"'$celular'",
-				"email"=>"'$email'",
-				"codcargo"=>"'$codcargo'",
-				"turno"=>"'$turno'",
-                "codcontrato"=>"'$codcontrato'",
-                "iniciocontrato"=>"'$iniciocontrato'",
-				"duracioncontrato"=>"'$duracioncontrato'",
-                
-				"foto"=>"'$foto'",
-                "curriculum"=>"'$curriculum'",
+$data->ChangeSheet(0);
+$i=0;
+$asistencia->vaciar();
+foreach ($data as $Key => $Row){
+    if($Row[0]!="N"){
+    /*echo "<pre>";
+    print_r($Row);
+    echo "</pre>";*/
+    
+    $valores=array(	"ci"=>"'".$Row[1]."'",
+				"fechaasistencia"=>"'".date("Y-m-d",strtotime($Row[2]))."'",
+				"horaingreso1"=>"'".$Row['4']."'",
+				"horasalida1"=>"'".$Row['5']."'",
+				"tiempotemprano1"=>"'".$Row['6']."'",
+				"tiempotarde1"=>"'".$Row['7']."'",
+				"horaingreso2"=>"'".$Row['8']."'",
+				"horasalida2"=>"'".$Row['9']."'",
+				"tiempotemprano2"=>"'".$Row['10']."'",
+				"tiempotarde2"=>"'".$Row['11']."'",
+                "horaingreso3"=>"'".$Row['12']."'",
+				"horasalida3"=>"'".$Row['13']."'",
+				"tiempotemprano3"=>"'".$Row['14']."'",
+				"tiempotarde3"=>"'".$Row['15']."'",
 				
 				);
-				$personal->insertar($valores);
-				
+           /* echo "<pre>";
+            print_r($valores);
+            echo "</pre>";      */
+				$asistencia->insertar($valores);
+    
+    }
 
-$id=$personal->last_id();
-$val=array("usuario"=>$id);
-$personal->actualizar($val,$id);
-*/
-$asistencia->vaciar();
+}
+
+/*$Sheets = $data -> Sheets();
+
+foreach ($Sheets as $Index => $Name){
+    $data -> ChangeSheet($Index);
+    foreach ($data as $Key => $Row){
+		//echo $Key.': ';
+        if ($Row)
+        {
+            print_r($Row);
+        }
+        else
+        {
+            //var_dump($Row);
+        }
+    }
+}*/
+
+//
 $mensaje[]="SUS DATOS SE GUARDARON CORRECTAMENTE";
 $titulo="Mensaje de Respuesta";
 $folder="../../";
